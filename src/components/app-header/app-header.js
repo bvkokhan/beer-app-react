b1c5d679e2a66eb9b17de6d3323003c021b796ef
Bogdan <bv.kokhan@gmail.com>
2021-04-26 15:23:23 +0300

    added reg form with walidation

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Modal} from 'react-bootstrap';
+import Form from '../form/form';
 
 import './app-header.css'
 
+
 const AppHeader = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
+        <div>
         <div className="app-header d-flex">
             <h1>Beer List App</h1>
+            <button className='btn btn-danger' onClick={handleShow}>Log In</button>
+        </div>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Log In</Modal.Title>
+            </Modal.Header> 
+            <Modal.Body>
+                <Form/>
+            </Modal.Body> 
+            <Modal.Footer>
+            <button className='btn btn-success' onClick={handleClose}>Submit</button> 
+            </Modal.Footer>
+
+        </Modal>
         </div>
     )
 }
